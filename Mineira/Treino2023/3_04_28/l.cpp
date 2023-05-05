033a0b1122e645f441bc684cd7383b58eb92ad1e
bernborgess <bernborgess@outlook.com>
2023-05-05 11:27:21 -0300

    try and upsolve

--- Mineira/Treino2023/3_04_28/l.cpp
+++ Mineira/Treino2023/3_04_28/l.cpp
@@ -6,8 +6,79 @@ using namespace std;
 #define endl '\n'
 #define umap unordered_map
 #define debug(x) cout << #x << " = " << x << endl
-#define int long long
 
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+// !LIB
+
+// Inverso Modular
+//
+// Computa o inverso de a modulo b
+// Se b eh primo, basta fazer
+// a^(b-2)
+
+ll inv(ll a, ll b) {
+  return a > 1 ? b - inv(b % a, a) * b / a : 1;
+}
+
+// Exponenciacao rapida
+//
+// (x^y mod m) em O(log(y))
+
+ll pow(ll x, ll y, ll m) {  // iterativo
+  ll ret = 1;
+  while (y) {
+    if (y & 1) ret = (ret * x) % m;
+    y >>= 1;
+    x = (x * x) % m;
+  }
+  return ret;
+}
+
+// Logaritmo Discreto
+//
+// Resolve logaritmo discreto com o algoritmo baby step giant step
+// Encontra o menor x tal que a^x = b (mod m)
+// Se nao tem, retorna -1
+//
+// O(sqrt(m) * log(sqrt(m))
+
+int dlog(int b, int a, int m) {
+  if (a == 0) return b ? -1 : 1;  // caso nao definido
+
+  a %= m, b %= m;
+  int k = 1, shift = 0;
+  while (1) {
+    int g = gcd(a, m);
+    if (g == 1) break;
+
+    if (b == k) return shift;
+    if (b % g) return -1;
+    b /= g, m /= g, shift++;
+    k = (ll)k * a / g % m;
+  }
+
+  int sq = sqrt(m) + 1, giant = 1;
+  for (int i = 0; i < sq; i++) giant = (ll)giant * a % m;
+
+  vector<pair<int, int>> baby;
+  for (int i = 0, cur = b; i <= sq; i++) {
+    baby.emplace_back(cur, i);
+    cur = (ll)cur * a % m;
+  }
+  sort(baby.begin(), baby.end());
+
+  for (int j = 1, cur = k; j <= sq; j++) {
+    cur = (ll)cur * giant % m;
+    auto it = lower_bound(baby.begin(), baby.end(), pair(cur, INF));
+    if (it != baby.begin() and (--it)->first == cur)
+      return sq * j - it->second + shift;
+  }
+
+  return -1;
+}
+// !LIB
+#if 0
 typedef tuple<int, int, int> t3;
 
 t3 gcd_ex(int a, int b) {
@@ -59,6 +130,7 @@ int log_bsgs(int a, int b, int p) {
   }
   return -1;
 }
+#endif
 
 void solve() {
   int a, b, s, p, v;
@@ -68,7 +140,10 @@ void solve() {
     return;
   }
   if (a == 1) {
-    int b_inv = inv_p(b, p);
+    // Computa o inverso de b modulo p
+    // Se p eh primo, basta fazer
+    // b^(p-2)
+    int b_inv = pow(b, p - 2, p);
     int n = ((v - s) * b_inv) % p;
     cout << n << endl;
     return;
@@ -77,10 +152,12 @@ void solve() {
     cout << "IMPOSSIBLE" << endl;
     return;
   }
-  int sab_i = inv_p((s * (a - 1) + b) % p, p) % p;
+  int sab_i = inv((s * (a - 1) + b) % p, p) % p;
+  // ll sab_i = pow((s * (a - 1) + b) % p, p + 2, p);
   if (sab_i < 0) sab_i += p;
-  int RHS = (((v * (a - 1)) % p + b) * sab_i) % p;
-  int n = log_bsgs(a, RHS, p);
+  ll RHS = (((v * (a - 1)) % p + b) * sab_i) % p;
+  // ll n = log_bsgs(a, RHS, p);
+  ll n = dlog(a, RHS, p);
   if (n == -1) {
     cout << "IMPOSSIBLE" << endl;
     return;
